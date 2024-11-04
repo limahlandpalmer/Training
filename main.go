185de6e68602d6bb6d2d7c9a2bce30e74d2a17c9
agusrio <asubandrio@gmail.com>
2024-11-04 14:40:26 +0700

    Commmit

--- main.go
+++ main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"fmt"
-	"TRAINING_04112024/utils"
+	"training_go/training01/utils"
 )
 
 func main() {
 
-	result := MagicSum(5)
+	result := utils.MagicSum(5)
 	fmt.Println("Nilai Akhir MagicSum", result)
 	m := 4 // Example input
-	result1 := MagicPow(m)
+	result1 := utils.MagicPow(m)
 	fmt.Println("Nilai Akhir MagicPow", result1)
 	l := 6 // Example input
-	result2 := MagicOdd(l)
+	result2 := utils.MagicOdd(l)
 	fmt.Println("Nilai Akhir MagicOdd", result2)
 	k := 3 // Example input
-	result3 := MagicGrade(k)
+	result3 := utils.MagicGrade(k)
 	fmt.Println("Nilai Akhir MagicGrade", k, result3)
 	o := 1 // Example input
-	result4 := MagicName(o)
+	result4 := utils.MagicName(o)
 	fmt.Println("Nilai Akhir MagicName", result4)
 	p := 3 // Example input
-	result5 := MagicTria(p)
+	result5 := utils.MagicTria(p)
 	fmt.Println("Nilai Akhir MagicTria", result5)
 	r := 5 // Example input
 	fmt.Printf("Before change: %d\n", r)
-	MagicChange(&r) // Pass the address of n
+	utils.MagicChange(&r) // Pass the address of n
 	fmt.Printf("After change: %d\n", r)
 }
---
+++ utils/readme.md
@@ -0,0 +1,2 @@
+
+Nama : Agus Subandrio
